5dbfbe69e52a9d4c62d061c78c69ca65dd51b289
MillionDollarBob <riccardo.c1995@outlook.com>
2023-04-04 15:01:39 +0200

    Esercizio base svolto, primo bonus in progress

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* Data una lista della spesa, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
+Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato, utilizzando uno dei due cicli scritti al punto precedente.
+Consigli:
+Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+Bonus:
+Aggiungere alla pagina HTML un campo di input testuale e un pulsante “aggiungi”: l’utente inserisce nell’input un prodotto da acquistare e quando clicca sul pulsante, il testo viene aggiunto alla lista.
+
+Super Bonus:
+Aggiungere a fianco ad ogni elemento della lista un pulsante “elimina”: quando l’utente clicca su uno di questi pulsanti, l’elemento corrispondente viene rimosso dalla lista.
+Buon lavoro a tutte e a tutti!! */
+
+/* Esercizio base
+let listaSpesa = ["farina", "pollo", "acqua", "frutta", "spinaci"];
+//let contatore = 0;
+
+//console.log(listaSpesa.lenght);
+
+for(let i = 0 ; i< listaSpesa.length ; i++){
+    console.log(i+1 + ". " + listaSpesa[i]);
+}
+let i =0;
+while(i < listaSpesa.length ){
+    console.log(i+1 + "- " + listaSpesa[i]);
+    i++
+}
+*/
+
+// bonus
+
